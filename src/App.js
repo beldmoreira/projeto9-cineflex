390bfd592e5706ace0b5bba6b6efba0ce399dbc7
Isabela <isabela_cmoreira@hotmail.com>
2022-03-23 13:51:50 -0300

    Added the 'Success' route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import Sessions from "./components/Sessions";
 import Seats from "./components/Seats";
+import Success from "./components/Success";
 
 export default function App (){
     return(
@@ -12,6 +13,7 @@ export default function App (){
                 <Route path="/" element={<Homepage/>}/> 
                 <Route path="/sessoes:idFilme" element={<Sessions/>} /> 
                 <Route path="/assentos:idSessao" element={<Seats/>} /> 
+                <Route path="/sucesso" element = {<Success/>}/>
             </Routes>
         </BrowserRouter>
     );
