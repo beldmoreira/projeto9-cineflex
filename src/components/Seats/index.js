91a390b5c6777f6ff1d8f947b1039aa46a9abafd
Isabela <isabela_cmoreira@hotmail.com>
2022-03-28 11:57:00 -0300

    Added  a link to the 'sucesso' screen in a button using useNavigate

--- src/components/Seats/index.js
+++ src/components/Seats/index.js
@@ -1,9 +1,16 @@
 import { Container, Input } from "./style";
 import useState from "react";
+import Button from "../Button";
+import { useNavigate } from "react-router";
 
 export default function Seats (){
     const [name, setName] = useState ("");
     const [cpf, setCpf,] = useState ("");
+    const navigate = useNavigate();
+
+    function DoneOrder() {
+        navigate('/sucesso');
+    }
     return (
     <>
     <Container>
@@ -18,6 +25,7 @@ export default function Seats (){
     <span>CPF do comprador:</span>
     <Input type = "text" placeholder="Digite seu CPF..." onChange={(e) => setCpf(e.target.value)} value = {cpf}/>
 
+    <Button onClick={DoneOrder}> Reservar assento(s) </Button>
     </>
     );
 }
