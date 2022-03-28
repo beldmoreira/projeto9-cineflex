24c556e3209b32d208b4dfc0c0b5efc9d32440a2
Isabela <isabela_cmoreira@hotmail.com>
2022-03-28 16:34:12 -0300

    Updated version of the App component with new import and functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import Sessions from "./components/Sessions";
@@ -6,14 +7,15 @@ import Seats from "./components/Seats";
 import Success from "./components/Success";
 
 export default function App (){
+    const [booking, setBooking] = useState(null);
     return(
         <BrowserRouter>
             <Navbar/>
             <Routes>
                 <Route path="/" element={<Homepage/>}/> 
-                <Route path="/sessoes:idFilme" element={<Sessions/>} /> 
-                <Route path="/assentos:idSessao" element={<Seats/>} /> 
-                <Route path="/sucesso" element = {<Success/>}/>
+                <Route path="/sessoes:idFilme" element={<Sessions />} /> 
+                <Route path="/assentos:idSessao" element={<Seats setBooking={setBooking}/>} /> 
+                <Route path="/sucesso" element = {<Success {...booking} setBooking={setBooking} />}/>
             </Routes>
         </BrowserRouter>
     );
