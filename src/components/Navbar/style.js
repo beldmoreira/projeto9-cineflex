1ceded06148477298e20235449c1270f84365799
Isabela <isabela_cmoreira@hotmail.com>
2022-03-28 16:38:29 -0300

    Navbar component last update (no major changes)

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -2,17 +2,16 @@ import styled from 'styled-components';
 
 const NavStyle = styled.header `
 height: 67px;
-width: 375px;
+width: 100%;
+top: 0;
+left: 0;
 background-color:#C3CFD9;
 display: flex;
 align-items: center;
 justify-content: center;
-width: 100%;
 position: fixed;
-z-index= 1
 `
 const NavFont = styled.span `
-font-family: Roboto;
 font-size: 34px;
 font-style: normal;
 font-weight: 400;
@@ -22,8 +21,6 @@ text-align: center;
 color: #E8833A;
 `
 
-
-    
 
 
 export{
