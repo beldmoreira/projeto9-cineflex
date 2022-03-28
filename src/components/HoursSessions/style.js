c46a603c0476ff6837249e9a85765582b8d4ab90
Isabela <isabela_cmoreira@hotmail.com>
2022-03-28 11:39:05 -0300

    Updated some styled components and deleted a few that were outdated

--- src/components/HoursSessions/style.js
+++ src/components/HoursSessions/style.js
@@ -1,24 +1,6 @@
 import styled from "styled-components";
 
-
-const Container = styled.div`
-height: 110px;
-display: flex;
-align-items: center;
-justify-content: center;
-`;
-
-const Font = styled.span`
-font-family: Roboto;
-font-size: 24px;
-font-style: normal;
-font-weight: 400;
-line-height: 28px;
-letter-spacing: 0.04em;
-text-align: center;
-color: #293845;
-`
-const HoursButton = styled.button `
+const HoursButton = styled.button`
 height: 43px;
 width: 82px;
 background-color: #E8833A;
@@ -30,8 +12,7 @@ letter-spacing: 0.02em;
 text-align: center;
 `;
 
-const PrintStyle = styled. span `
-font-family: Roboto;
+const PrintStyle = styled.span `
 font-size: 20px;
 font-style: normal;
 font-weight: 400;
@@ -41,8 +22,6 @@ text-align: left;
 `;
 export{
     HoursButton,
-    Container,
-    Font,
     PrintStyle
-
 }
+
---
+++ src/components/Sessions/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div `
+height: 110px;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const MovieSession = styled.div`
+display: flex;
+flex-direction: column;
+padding: 0 23px;
+margin-bottom: 117px;
+`;
+export{
+    Container,
+    MovieSession
+}
